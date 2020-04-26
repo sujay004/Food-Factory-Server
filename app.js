a59e9f5e827a6346bd9f66b1362e4ee04c3e94cc
sujay004 <40460405+sujay004@users.noreply.github.com>
2020-04-26 11:43:18 +0530

    es-lint implementation

--- app.js
+++ app.js
@@ -1,57 +1,55 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var bodyParser = require('body-parser');
+require('custom-env').env('dev', './environment/');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const http = require('http');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+const usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
 
-var indexRouter = require('./routes/index');
-var http = require('http');
-var usersRouter = require('./routes/users');
-var app = express();
-
-var swaggerUi = require('swagger-ui-express'),
-    swaggerDocument = require('./swagger.json');
-    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+const app = express();
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api/v1', indexRouter);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(
   bodyParser.urlencoded({
-    extended: true
-  })
+    extended: true,
+  }),
 );
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/api/', indexRouter);
 app.use('/api/', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use((req, res, next) => {
+  next(createError(404, 'API Not Found'));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
 
-var server = http.createServer(app);
-server.listen(3000,function(){
-  console.log('Server is running on 3000');
+const server = http.createServer(app);
+const { PORT } = process.env;
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,221 +1,239 @@
-var express = require('express');
-var nodemailer = require('nodemailer');
-
-const MongoClient = require('mongodb').MongoClient;
-var ObjectId = require('mongodb').ObjectID;
-var url = 'mongodb://localhost:27017';
-var dbName = 'food_factory';
-
-
-var MONGODB;
-MongoClient.connect(url, function (err, client) {
-  console.log("MONGODB Connected successfully to server");
-  MONGODB = client.db(dbName);
-});
-
-
-var transporter = nodemailer.createTransport({
+const express = require('express');
+const nodemailer = require('nodemailer');
+const bcrypt = require('bcrypt');
+const createError = require('http-errors');
+const { MongoClient } = require('mongodb');
+// const ObjectId = require('mongodb').ObjectID;
+const url = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}`;
+const dbName = process.env.DB_NAME;
+let MONGODB;
+const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: 'xxxx@gmail.com',
-    pass: 'xxxxxx'
+    pass: 'xxxxxx',
+  },
+});
+const router = express.Router();
+const saltRounds = 10;
+
+
+MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+  if (!client) {
+    throw err;
+  } else {
+    MONGODB = client.db(dbName);
+    console.log('MONGODB Connected successfully to server');
   }
 });
 
 
-
-
-const bcrypt = require('bcrypt');
-var router = express.Router();
-
-const saltRounds = 10;
-
-
-/* GET users listing. */
-router.post('/user/create', function (req, res) {
-
-  var userData = req.body;
-  console.log(userData)
-  var collection = MONGODB.collection('users');
-
-  collection.find({
-    user_name: userData.user_name,
-    email_id: userData.email_id,
-    mobile_number: userData.mobile_number
-  }).toArray((err, body) => {
-    if (err) {
-      res.send(403);
-    } else {
-      console.log(body)
-      if (body.length > 0) {
-        res.sendStatus(403);
-      } else {
-        collection.insertOne({
-          first_name: userData.first_name,
-          last_name: userData.last_name,
-          user_name: userData.user_name,
-          password: bcrypt.hashSync(userData.password, saltRounds),
-          email_id: userData.email_id,
-          mobile_number: userData.mobile_number,
-          created_at: new Date(),
-          updated_at: new Date(),
-          active: 1
-        }, (err, body) => {
-          if (err) {
-            res.sendStatus(403);
-          } else {
-            res.send(200);
-          }
-        });
-      }
-    }
-  });
-
-});
-
-
-router.get('/user/authentication', (req, res) => {
-  var user_name = req.query.user_name;
-  var password = req.query.password;
-  console.log(user_name, password);
-
-  const hash = bcrypt.hashSync(password, saltRounds);
-  console.log(hash)
-  var collection = MONGODB.collection('users');
-  collection.find({
-    user_name: user_name
-    // password: hash
-  }).toArray((err, body) => {
-    if (err) {
-      res.sendStatus(403);
-    } else {
-      console.log(body)
-      bcrypt.compare(password, body[0].password).then(function (result) {
-        if (result == true) {
-          res.sendStatus(200);
+router.post('/user/create', (req, res, next) => {
+  try {
+    const userData = req.body;
+    if (req.body !== undefined) {
+      const collection = MONGODB.collection('users');
+      collection.find({
+        user_name: userData.user_name,
+        email_id: userData.email_id,
+        mobile_number: userData.mobile_number,
+      }).toArray((err, body) => {
+        if (err) {
+          throw createError(403, 'Db Operation Failed');
+        } else if (body.length > 0) {
+          res.status(200).json({ message: 'User Exist..!! Try Different..!!' });
         } else {
-          res.sendStatus(403);
+          collection.insertOne({
+            first_name: userData.first_name,
+            last_name: userData.last_name,
+            user_name: userData.user_name,
+            password: bcrypt.hashSync(userData.password, saltRounds),
+            email_id: userData.email_id,
+            mobile_number: userData.mobile_number,
+            created_at: new Date(),
+            updated_at: new Date(),
+            active: 1,
+            // eslint-disable-next-line no-shadow
+          }, (err) => {
+            if (err) {
+              throw createError(403, 'Db Operation Failed');
+            } else {
+              res.status(200).json({ message: 'User Created Successfully..!!' });
+            }
+          });
         }
       });
-
+    } else {
+      throw createError(403, 'Missing Body Value');
     }
-  });
+  } catch (err) {
+    next(err);
+  }
 });
 
 
-router.get('/user/resetPassword', (req, res) => {
-  var email_id = req.query.email_id;
-  var randomPassword = Math.random().toString(36).substring(7);
-  const hash = bcrypt.hashSync(randomPassword, saltRounds);
-  var collection = MONGODB.collection('users');
-  collection.update(
-    { email_id: email_id },
-    {
-      $set: {
-        password: hash,
-        updated_at: new Date()
-      }
-    }, (err, body) => {
-      if (err) {
-        res.sendStatus(403);
-      } else {
-        var mailOptions = {
-          from: 'xxxx@gmail.com',
-          to: 'yyyy@gmail.com',
-          subject: 'About Password Reset',
-          text: randomPassword
-        };
-        transporter.sendMail(mailOptions, function (error, info) {
-          if (error) {
-            console.log(error);
+router.get('/user/authentication', (req, res, next) => {
+  try {
+    const userName = req.query.user_name;
+    const { password } = req.query;
+    if (userName !== undefined && password !== undefined) {
+      const collection = MONGODB.collection('users');
+      collection.find({
+        user_name: userName,
+      }).toArray((err, body) => {
+        if (err) {
+          throw createError(403, 'Db Operation Failed');
+        } else {
+          bcrypt.compare(password, body[0].password).then((result) => {
+            if (result === true) {
+              res.status(200).json({ message: 'Login Success..!!' });
+            } else {
+              res.status(422).json({ message: 'Incorrect Credentials..!!' });
+            }
+          });
+        }
+      });
+    } else {
+      throw createError(403, 'Param Values Mismatch');
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+
+router.get('/user/resetPassword', (req, res, next) => {
+  try {
+    const emailId = req.query.email_id;
+    if (emailId !== undefined) {
+      const randomPassword = Math.random().toString(36).substring(7);
+      const hash = bcrypt.hashSync(randomPassword, saltRounds);
+      const collection = MONGODB.collection('users');
+      collection.update(
+        { email_id: emailId },
+        {
+          $set: {
+            password: hash,
+            updated_at: new Date(),
+          },
+        }, (err) => {
+          if (err) {
+            throw createError(403, 'Db Operation Failed');
           } else {
-            console.log('Email sent: ' + info.response);
+            const mailOptions = {
+              from: 'xxxx@gmail.com',
+              to: 'yyyy@gmail.com',
+              subject: 'About Password Reset',
+              text: randomPassword,
+            };
+            transporter.sendMail(mailOptions, (error, info) => {
+              if (error) {
+                // eslint-disable-next-line no-console
+                console.log(error);
+              } else {
+                // eslint-disable-next-line no-console
+                console.log(`Email sent: ${info.response}`);
+              }
+            });
+            res.sendStatus(200);
           }
-        });
-        res.sendStatus(200);
-      }
-    });
-});
-
-
-
-router.get('/user/deactive', (req, res) => {
-  var user_name = req.query.user_name;
-
-  var collection = MONGODB.collection('users');
-  collection.update({
-    user_name: user_name
-  },
-    {
-      $set: {
-        active: 0
-      }
-    }, (err, body) => {
-      if (err) {
-        res.sendStatus(403);
-      } else {
-        res.sendStatus(200);
-      }
-    });
-});
-
-
-
-
-
-
-router.post('/order/create', function (req, res) {
-  var orderData = req.body;
-
-  var collection = MONGODB.collection('orders');
-
-  collection.insert({
-    user_id: orderData.user_id,
-    order_name: orderData.order_name,
-    food_id: orderData.food_id,
-    qty: orderData.qty,
-    offer_price: orderData.offer_price,
-    original_price: orderData.original_price,
-    offer_name: orderData.offer_name,
-    active: 1
-  }, (err, body) => {
-    if (err) {
-      res.sendStatus(403);
+        },
+      );
     } else {
-      res.send(200);
+      throw createError(403, 'Param Mismatch');
     }
-  });
-
+  } catch (err) {
+    next(err);
+  }
 });
 
 
-
-router.get('/order/getAllOrder', function (req, res) {
-
-  var user_id = req.query.user_id;
-  console.log(user_id)
-  var collection = MONGODB.collection('orders');
-
-  collection.find({
-    user_id: user_id,
-    active: 1
-  }).toArray((err, body) => {
-    if (err) {
-      res.sendStatus(403);
+router.get('/user/deactive', (req, res, next) => {
+  try {
+    const userName = req.query.user_name;
+    if (userName !== undefined) {
+      const collection = MONGODB.collection('users');
+      collection.update({
+        user_name: userName,
+      },
+      {
+        $set: {
+          active: 0,
+        },
+      }, (err) => {
+        if (err) {
+          throw createError(403, 'Db Operation Failed');
+        } else {
+          res.status(200).json({ message: 'User Deactivate Success..!!' });
+        }
+      });
     } else {
-      res.send(body);
+      throw createError(403, 'Param Mismatch');
     }
-  });
-
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.get('/ingredient/getThresoldQty', function (req, res) {
 
-  var collection = MONGODB.collection('ingredients');
+router.post('/order/create', (req, res, next) => {
+  try {
+    const orderData = req.body;
+    if (orderData !== undefined) {
+      const collection = MONGODB.collection('orders');
+      collection.insert({
+        user_id: orderData.user_id,
+        order_name: orderData.order_name,
+        food_id: orderData.food_id,
+        qty: orderData.qty,
+        offer_price: orderData.offer_price,
+        original_price: orderData.original_price,
+        offer_name: orderData.offer_name,
+        active: 1,
+      }, (err) => {
+        if (err) {
+          throw createError(403, 'Db Operation Failed');
+        } else {
+          res.status(200).json({ message: 'Order created successfully..!!' });
+        }
+      });
+    } else {
+      throw createError(403, 'Body Value Missing');
+    }
+  } catch (err) {
+    next(err);
+  }
+});
 
+
+router.get('/order/getAllOrder', (req, res, next) => {
+  try {
+    const userId = req.query.user_id;
+    if (userId !== undefined) {
+      const collection = MONGODB.collection('orders');
+      collection.find({
+        user_id: userId,
+        active: 1,
+      }).toArray((err, body) => {
+        if (err) {
+          throw createError(403, 'Db Operation Failed');
+        } else {
+          res.send(body);
+        }
+      });
+    } else {
+      throw createError(403, 'Param Value Mismatch');
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/ingredient/getThresoldQty', (req, res, next) => {
+  const collection = MONGODB.collection('ingredients');
   collection.find({ remaining_qty: { $lt: 1000 } }).toArray((err, body) => {
     if (err) {
-      res.sendStatus(403);
+      next(createError(403, 'Db Operation Failed'));
     } else {
       res.send(body);
     }
@@ -223,39 +241,38 @@ router.get('/ingredient/getThresoldQty', function (req, res) {
 });
 
 
-
-router.get('/ingredient/getBySameVendor', function (req, res) {
-  var vendor_id = req.query.vendor_id;
-  console.log('hello--', vendor_id);
-
-  var collection = MONGODB.collection('ingredients');
-
-  collection.find({
-    vendor_id: vendor_id,
-  }).toArray((err, body) => {
-    if (err) {
-      res.sendStatus(403);
+router.get('/ingredient/getBySameVendor', (req, res, next) => {
+  try {
+    const vendorId = req.query.vendor_id;
+    if (vendorId !== undefined) {
+      const collection = MONGODB.collection('ingredients');
+      collection.find({
+        vendor_id: vendorId,
+      }).toArray((err, body) => {
+        if (err) {
+          throw createError(403, 'Db Operation Failed');
+        } else {
+          res.send(body);
+        }
+      });
     } else {
-      res.send(body);
+      throw createError(403, 'Param Value Mismatch');
     }
-  });
-
-  // res.render('index', { title: 'Express' });
+  } catch (err) {
+    next(err);
+  }
 });
 
 
-router.get('/food/getListOverSellingCost', function (req, res) {
-
-  var collection = MONGODB.collection('foods');
-
-  collection.find({ $expr: { $gt: ["$production_cost", "$selling_cost"] } }).toArray((err, body) => {
+router.get('/food/getListOverSellingCost', (req, res, next) => {
+  const collection = MONGODB.collection('foods');
+  collection.find({ $expr: { $gt: ['$production_cost', '$selling_cost'] } }).toArray((err, body) => {
     if (err) {
-      res.sendStatus(403);
+      next(createError(403, 'Db Operation Failed'));
     } else {
       res.send(body);
     }
   });
-
 });
 
 
